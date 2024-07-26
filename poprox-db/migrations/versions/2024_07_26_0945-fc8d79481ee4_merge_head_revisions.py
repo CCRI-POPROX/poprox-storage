961dfafc91c7a7d6a3598c372c6069a5a35c82b6
Karl Higley <kmhigley@gmail.com>
2024-07-26 10:00:20 -0400

    Merge database revision heads

---
+++ poprox-db/migrations/versions/2024_07_26_0945-fc8d79481ee4_merge_head_revisions.py
@@ -0,0 +1,26 @@
+"""merge head revisions
+
+Revision ID: fc8d79481ee4
+Revises: 14be38931ae0, 5d566fff8aaa
+Create Date: 2024-07-26 09:45:33.246538
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'fc8d79481ee4'
+down_revision: Union[str, None] = ('14be38931ae0', '5d566fff8aaa')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
