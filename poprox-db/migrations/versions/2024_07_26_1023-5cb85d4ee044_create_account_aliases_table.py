4e35f73ba8b06324355e3f7c798ef5f90d0ac413
Karl Higley <kmhigley@gmail.com>
2024-07-26 10:38:36 -0400

    Add migration to create `account_aliases` table

---
+++ poprox-db/migrations/versions/2024_07_26_1023-5cb85d4ee044_create_account_aliases_table.py
@@ -0,0 +1,56 @@
+"""create account aliases table
+
+Revision ID: 5cb85d4ee044
+Revises: 653b309492ae
+Create Date: 2024-07-26 10:23:16.288178
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "5cb85d4ee044"
+down_revision: Union[str, None] = "653b309492ae"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "account_aliases",
+        sa.Column(
+            "alias_id",
+            sa.UUID,
+            primary_key=True,
+            server_default=sa.text("gen_random_uuid()"),
+        ),
+        sa.Column("account_id", sa.UUID, nullable=False),
+        sa.Column("dataset_id", sa.UUID, nullable=False),
+        sa.Column("created_at", sa.DateTime, nullable=False, server_default=sa.text("NOW()")),
+    )
+
+    op.create_foreign_key(
+        "fk_account_aliases_account_id",
+        "account_aliases",
+        "accounts",
+        ["account_id"],
+        ["account_id"],
+    )
+
+    op.create_foreign_key(
+        "fk_account_aliases_dataset_id",
+        "account_aliases",
+        "datasets",
+        ["dataset_id"],
+        ["dataset_id"],
+    )
+
+
+def downgrade() -> None:
+    op.drop_constraint("fk_account_aliases_dataset_id", "account_aliases", type_="foreignkey")
+    op.drop_constraint("fk_account_aliases_account_id", "account_aliases", type_="foreignkey")
+    op.drop_table("account_aliases")
