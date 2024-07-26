60e73f5a53e3c9f84c30fb78c75347d3bfb793fb
Daniel Kluver <kluve018@umn.edu>
2024-07-26 15:02:02 -0500

    fix pre-commit issues

--- poprox-db/migrations/versions/2024_07_26_1352-98ff1c3ebe33_add_web_login_table.py
+++ poprox-db/migrations/versions/2024_07_26_1352-98ff1c3ebe33_add_web_login_table.py
@@ -7,8 +7,8 @@ Create Date: 2024-07-26 13:52:10.169410
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects.postgresql import JSONB
 
 # revision identifiers, used by Alembic.
