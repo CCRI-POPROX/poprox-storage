6c7279ecfb7b8c3b604d02b24f8f154b34843e4d
Karl Higley <kmhigley@gmail.com>
2024-07-29 14:54:45 -0400

    Apply additional linter formatting

--- poprox-db/migrations/versions/2024_07_29_1338-f5973e9cf00f_merge_revisions_heads.py
+++ poprox-db/migrations/versions/2024_07_29_1338-f5973e9cf00f_merge_revisions_heads.py
@@ -5,15 +5,12 @@ Revises: 085f4b0b94a6, 98ff1c3ebe33
 Create Date: 2024-07-29 13:38:37.108840
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = 'f5973e9cf00f'
-down_revision: Union[str, None] = ('085f4b0b94a6', '98ff1c3ebe33')
+revision: str = "f5973e9cf00f"
+down_revision: Union[str, None] = ("085f4b0b94a6", "98ff1c3ebe33")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
