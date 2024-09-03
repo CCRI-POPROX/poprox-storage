e41db9a77c0c85bb5025f68eb4391ee6219b71b5
Karl Higley <kmhigley@gmail.com>
2024-09-03 15:18:14 -0400

    Use the database credentials in the GH Action for the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,11 @@ import os
 import pytest
 from sqlalchemy import create_engine
 
+db_user = os.environ.get("POPROX_DB_USER", "postgres")
 db_password = os.environ.get("POPROX_DB_PASSWORD", "")
 db_port = os.environ.get("POPROX_DB_PORT", "5433")
 
-DEFAULT_PG_URL = f"postgresql://postgres:{db_password}@127.0.0.1:{db_port}/poprox"
+DEFAULT_PG_URL = f"postgresql://{db_user}:{db_password}@127.0.0.1:{db_port}/poprox"
 
 
 @pytest.fixture(scope="session")
