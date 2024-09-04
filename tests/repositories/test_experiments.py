1879e9ac350b7468ee5d14493e0c8c5579f61abf
Karl Higley <kmhigley@gmail.com>
2024-09-04 11:41:01 -0400

    Create the experiment owner account if it doesn't already exist

--- tests/repositories/test_experiments.py
+++ tests/repositories/test_experiments.py
@@ -1,5 +1,7 @@
 from uuid import UUID
 
+import sqlalchemy
+
 from poprox_concepts.domain import Account
 from poprox_storage.concepts.manifest import manifest_to_experiment, parse_manifest_toml
 from poprox_storage.paths import project_root
@@ -15,18 +17,22 @@ def test_store_experiment():
 
     with DB_ENGINE.connect() as conn:
         account_repo = DbAccountRepository(conn)
-        account_id = account_repo.store_account(
-            Account(
-                account_id=experiment.owner.members[0],
-                email="example@example.com",
-                source="test",
-                status="test",
-            )
-        )
-        assert isinstance(account_id, UUID)
-        assert str(account_id) == "1936ac91-daf0-4af8-9aa1-53a170c514aa"
-
         experiment_repo = DbExperimentRepository(conn)
+
+        try:
+            account_id = account_repo.store_account(
+                Account(
+                    account_id=experiment.owner.members[0],
+                    email="example@example.com",
+                    source="test",
+                    status="test",
+                )
+            )
+            assert isinstance(account_id, UUID)
+            assert str(account_id) == "1936ac91-daf0-4af8-9aa1-53a170c514aa"
+        except sqlalchemy.exc.IntegrityError:
+            ...
+
         experiment_id = experiment_repo.store_experiment(experiment)
 
         assert isinstance(experiment_id, UUID)
