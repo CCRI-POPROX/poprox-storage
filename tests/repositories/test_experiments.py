8a0c773aefea65342b2f760566c52297f85780f3
Karl Higley <kmhigley@gmail.com>
2024-09-04 10:34:40 -0400

    Create account for experiment owner

--- tests/repositories/test_experiments.py
+++ tests/repositories/test_experiments.py
@@ -1,8 +1,9 @@
 from uuid import UUID
 
+from poprox_concepts.domain import Account
 from poprox_storage.concepts.manifest import manifest_to_experiment, parse_manifest_toml
 from poprox_storage.paths import project_root
-from poprox_storage.repositories import DbExperimentRepository
+from poprox_storage.repositories import DbAccountRepository, DbExperimentRepository
 from poprox_storage.repositories.data_stores.db import DB_ENGINE
 
 
@@ -13,6 +14,16 @@ def test_store_experiment():
     experiment = manifest_to_experiment(manifest)
 
     with DB_ENGINE.connect() as conn:
+        account_repo = DbAccountRepository(conn)
+        account_repo.store_account(
+            Account(
+                account_id=experiment.owner.members[0],
+                email="example@example.com",
+                source="test",
+                status="test",
+            )
+        )
+
         experiment_repo = DbExperimentRepository(conn)
         experiment_id = experiment_repo.store_experiment(experiment)
 
