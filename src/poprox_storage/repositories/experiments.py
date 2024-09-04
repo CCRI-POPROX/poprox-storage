1ef6939b54189b2bc37af7de244174b8f53336f0
Karl Higley <kmhigley@gmail.com>
2024-09-04 10:27:21 -0400

    Fix method call

--- src/poprox_storage/repositories/experiments.py
+++ src/poprox_storage/repositories/experiments.py
@@ -175,7 +175,7 @@ class DbExperimentRepository(DatabaseRepository):
     def _insert_expt_team(self, team: Team) -> UUID | None:
         team_id = self._insert_model("teams", team, exclude={"members"}, commit=False)
         for account_id in team.members:
-            self._upsert_and_return_id(team_id, account_id, commit=False)
+            self._insert_team_membership(team_id, account_id)
         return team_id
 
     def _insert_team_membership(self, team_id: UUID, account_id: UUID) -> UUID | None:
