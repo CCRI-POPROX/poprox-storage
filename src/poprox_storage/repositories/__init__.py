904a6f690ea87eff86c66b46e819c525ced8bd7b
Karl Higley <kmhigley@gmail.com>
2024-10-04 14:58:25 -0400

    Make repository injection work with S3 repos

--- src/poprox_storage/repositories/__init__.py
+++ src/poprox_storage/repositories/__init__.py
@@ -13,6 +13,36 @@ from poprox_storage.repositories.experiments import (
 from poprox_storage.repositories.images import DbImageRepository, S3ImageRepository
 from poprox_storage.repositories.newsletters import DbNewsletterRepository
 
+
+def inject_repos(handler):
+    from functools import wraps
+    from typing import get_type_hints
+
+    from poprox_storage.aws import DB_ENGINE
+    from poprox_storage.repositories.data_stores.db import DatabaseRepository
+    from poprox_storage.repositories.data_stores.s3 import S3Repository
+
+    @wraps(handler)
+    def wrapper(event, context):
+        params: dict[str, type] = get_type_hints(handler)
+        # remove event, context, and return type if they were annotated.
+        params.pop("event", None)
+        params.pop("context", None)
+        params.pop("return", None)
+
+        with DB_ENGINE.connect() as conn:
+            repos = dict()
+            for param, class_obj in params.items():
+                if class_obj in DatabaseRepository._repository_types:
+                    repos[param] = class_obj(conn)
+                elif class_obj in S3Repository._repository_types:
+                    repos[param] = class_obj(conn)
+
+            return handler(event, context, **repos)
+
+    return wrapper
+
+
 __all__ = [
     "DbAccountRepository",
     "DbAccountInterestRepository",
--- src/poprox_storage/repositories/data_stores/s3.py
+++ src/poprox_storage/repositories/data_stores/s3.py
@@ -1,9 +1,38 @@
+from functools import wraps
+from typing import get_type_hints
+
 from smart_open import open as smart_open
+
+
+def inject_s3_repos(handler):
+    @wraps(handler)
+    def wrapper(event, context):
+        params: dict[str, type] = get_type_hints(handler)
+        # remove event, context, and return type if they were annotated.
+        params.pop("event", None)
+        params.pop("context", None)
+        params.pop("return", None)
+
+        repos = dict()
+        for param, class_obj in params.items():
+            if class_obj in S3Repository._repository_types:
+                repos[param] = class_obj()
+
+        return handler(event, context, **repos)
+
+    return wrapper
 
 
 class S3Repository:
     def __init__(self, bucket_name):
         self.bucket_name: str = bucket_name
+
+    def __init_subclass__(cls, *args, **kwargs):
+        """
+        Gets called once for each loaded class that sub-classes DatabaseRepository
+        """
+
+        cls._repository_types.add(cls)
 
     def _get_s3_file(self, key):
         load_path = f"s3://{self.bucket_name}/{key}"
