3431caa33384316b1b72c468f037862a0e212088
Karl Higley <kmhigley@gmail.com>
2024-10-07 09:00:35 -0400

    Update the repository injection test

--- tests/test_injection.py
+++ tests/test_injection.py
@@ -1,10 +1,8 @@
-from poprox_storage.repositories import DbArticleRepository
-from poprox_storage.repositories.data_stores.db import inject_repos
-from poprox_storage.repositories.newsletters import DbNewsletterRepository
+from poprox_storage.repositories import DbNewsletterRepository, S3ArticleRepository, inject_repos
 
 
 @inject_repos
-def example(event, context, article: DbArticleRepository, newsletter_repo: DbNewsletterRepository):
+def example(event, context, article: S3ArticleRepository, newsletter_repo: DbNewsletterRepository):
     return article, newsletter_repo
 
 
@@ -12,5 +10,5 @@ def test_repositories():
     retval = example({}, {})
     assert isinstance(retval, tuple)
     assert len(retval) == 2
-    assert isinstance(retval[0], DbArticleRepository)
+    assert isinstance(retval[0], S3ArticleRepository)
     assert isinstance(retval[1], DbNewsletterRepository)
