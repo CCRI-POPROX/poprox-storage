7df76c0d35e4ce1bb6e6b42f7762b572a0993f38
Karl Higley <kmhigley@gmail.com>
2024-10-31 12:59:36 -0400

    Add article text to `Article`s

---
+++ poprox-db/migrations/versions/2024_10_31_1257-c23b2818e869_add_text_column_to_articles_table.py
@@ -0,0 +1,26 @@
+"""Add text column to articles table
+
+Revision ID: c23b2818e869
+Revises: 965cca438fba
+Create Date: 2024-10-31 12:57:14.134248
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "c23b2818e869"
+down_revision: Union[str, None] = "965cca438fba"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("articles", sa.Column("text", sa.String, nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column("articles", "text")
