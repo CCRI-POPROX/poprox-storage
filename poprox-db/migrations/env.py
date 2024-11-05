a6db6e8d6d080b1ab61bd45e004a0b1fbce90aaa
Karl Higley <khigley@umn.edu>
2024-11-05 10:41:53 -0500

    Update poprox-db/migrations/env.py

--- poprox-db/migrations/env.py
+++ poprox-db/migrations/env.py
@@ -27,7 +27,7 @@ target_metadata = None
 # ... etc.
 
 db_user = os.environ.get("POPROX_DB_USER", "postgres")
-db_password = os.environ.get("POPROX_DB_PASSWORD", "thisisapoproxpassword")
+db_password = os.environ.get("POPROX_DB_PASSWORD", "")
 db_host = os.environ.get("POPROX_DB_HOST", "127.0.0.1")
 db_port = os.environ.get("POPROX_DB_PORT", 5433)
 db_name = os.environ.get("POPROX_DB_NAME", "poprox")
