cef516ed3a60df5017f7f7449a19ee8593b501b3
Aishwarya Satwani <aishwaryasatwani@Aishwaryas-MacBook-Pro.local>
2024-11-11 12:42:05 -0700

    pre-commit command

--- poprox-db/migrations/versions/2024_10_29_1406-f8667df4b049_merge_head_revisions.py
+++ poprox-db/migrations/versions/2024_10_29_1406-f8667df4b049_merge_head_revisions.py
@@ -7,9 +7,8 @@ Create Date: 2024-10-29 14:06:37.323505
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'f8667df4b049'
