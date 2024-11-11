c6e5186ebd7b56fb251bb8f401e3cc96a6e4c630
Aishwarya Satwani <aishwaryasatwani@Aishwaryas-MacBook-Pro.local>
2024-11-11 12:53:04 -0700

    unused imports removed

--- poprox-db/migrations/versions/2024_10_29_1406-f8667df4b049_merge_head_revisions.py
+++ poprox-db/migrations/versions/2024_10_29_1406-f8667df4b049_merge_head_revisions.py
@@ -5,14 +5,12 @@ Revises: 0f5739c4d79d, 9bd2ccb216a6
 Create Date: 2024-10-29 14:06:37.323505
 
 """
+
 from typing import Sequence, Union
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
-revision: str = 'f8667df4b049'
-down_revision: Union[str, None] = ('0f5739c4d79d', '9bd2ccb216a6')
+revision: str = "f8667df4b049"
+down_revision: Union[str, None] = ("0f5739c4d79d", "9bd2ccb216a6")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
