19e70a60515e2c013a614226b851797fffc4ab32
Karl Higley <kmhigley@gmail.com>
2024-12-16 15:07:07 -0500

    Update the experiment repository test to use the new repo classes

--- tests/repositories/test_experiments.py
+++ tests/repositories/test_experiments.py
@@ -1,11 +1,16 @@
-from uuid import UUID, uuid4
+from uuid import UUID
 
 import sqlalchemy
 
 from poprox_concepts.domain import Account
 from poprox_storage.concepts.manifest import manifest_to_experiment, parse_manifest_toml
 from poprox_storage.paths import project_root
-from poprox_storage.repositories import DbAccountRepository, DbExperimentRepository
+from poprox_storage.repositories import (
+    DbAccountRepository,
+    DbDatasetRepository,
+    DbExperimentRepository,
+    DbTeamRepository,
+)
 from poprox_storage.repositories.data_stores.db import DB_ENGINE
 from tests import clear_tables
 
@@ -35,22 +40,26 @@ def test_store_experiment():
         )
 
         account_repo = DbAccountRepository(conn)
+        dataset_repo = DbDatasetRepository(conn)
         experiment_repo = DbExperimentRepository(conn)
+        team_repo = DbTeamRepository(conn)
+
+        account = Account(
+            account_id=experiment.owner.members[0],
+            email="example@example.com",
+            source="test",
+            status="test",
+        )
 
         try:
-            account_id = account_repo.store_account(
-                Account(
-                    account_id=experiment.owner.members[0],
-                    email="example@example.com",
-                    source="test",
-                    status="test",
-                )
-            )
+            account_id = account_repo.store_account(account)
             assert isinstance(account_id, UUID)
             assert str(account_id) == "1936ac91-daf0-4af8-9aa1-53a170c514aa"
         except sqlalchemy.exc.IntegrityError:
             ...
 
-        experiment_id = experiment_repo.store_experiment(experiment, [], uuid4())
+        experiment.owner.team_id = team_repo.store_team(experiment.owner)
+        dataset_id = dataset_repo.store_new_dataset([account], experiment.owner)
+        experiment_id = experiment_repo.store_experiment(experiment, [], dataset_id)
 
         assert isinstance(experiment_id, UUID)
