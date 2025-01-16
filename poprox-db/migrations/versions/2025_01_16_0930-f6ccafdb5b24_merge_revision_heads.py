4c571b858f11fdeb69dfd829ec4a58a0a6e25889
Daniel Kluver <kluve018@umn.edu>
2025-01-16 13:23:51 -0600

    make the linter happy

--- poprox-db/migrations/versions/2025_01_16_0930-f6ccafdb5b24_merge_revision_heads.py
+++ poprox-db/migrations/versions/2025_01_16_0930-f6ccafdb5b24_merge_revision_heads.py
@@ -8,9 +8,6 @@ Create Date: 2025-01-16 09:30:17.947408
 
 from typing import Sequence, Union
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision: str = "f6ccafdb5b24"
 down_revision: Union[str, None] = ("adf30c8ce87e", "58deb24f5008")
