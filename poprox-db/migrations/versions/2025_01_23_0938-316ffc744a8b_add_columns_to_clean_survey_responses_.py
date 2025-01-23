b172b555eef3137e0748c6a3ce2087938ebeffb7
Karl Higley <khigley@umn.edu>
2025-01-23 09:46:38 -0500

    Add columns to `qualtrics_clean_responses` table

---
+++ poprox-db/migrations/versions/2025_01_23_0938-316ffc744a8b_add_columns_to_clean_survey_responses_.py
@@ -0,0 +1,49 @@
+"""Add columns to clean survey responses table
+
+Revision ID: 316ffc744a8b
+Revises: f6ccafdb5b24
+Create Date: 2025-01-23 09:38:51.366350
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = '316ffc744a8b'
+down_revision: Union[str, None] = 'f6ccafdb5b24'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("qualtrics_clean_responses", sa.Column("account_id", sa.UUID, nullable=False))
+    op.add_column("qualtrics_clean_responses", sa.Column("survey_id", sa.UUID, nullable=False))
+    op.add_column("qualtrics_clean_responses", sa.Column("qualtrics_id", sa.UUID, nullable=False))
+    op.add_column("qualtrics_clean_responses", sa.Column("survey_code", sa.UUID, nullable=True))
+
+    op.create_foreign_key(
+        "fk_qualtrics_clean_responses_account_id",
+        "qualtrics_clean_responses",
+        "accounts",
+        ["account_id"],
+        ["account_id"],
+    )
+
+    op.create_foreign_key(
+        "fk_qualtrics_clean_responses_survey_id",
+        "qualtrics_clean_responses",
+        "qualtrics_surveys",
+        ["survey_id"],
+        ["survey_id"],
+    )
+
+def downgrade() -> None:
+    op.drop_constraint("fk_survey_calendar_survey_id", "qualtrics_survey_calendar", type_="foreignkey")
+    op.drop_constraint("fk_qualtrics_clean_responses_account_id", "qualtrics_clean_responses", type_="foreignkey")
+
+    op.drop_column("qualtrics_clean_responses", "survey_code")
+    op.drop_column("qualtrics_clean_responses", "qualtrics_id")
+    op.drop_column("qualtrics_clean_responses", "survey_id")
+    op.drop_column("qualtrics_clean_responses", "account_id")
