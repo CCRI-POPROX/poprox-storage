8979e133fe713712ffceed11477bf1c430180681
Daniel Kluver <kluve018@umn.edu>
2025-02-17 10:50:16 -0600

    Update poprox-db/migrations/versions/2025_02_06_1339-6da1373105ef_add_column_for_qualtrics_question_.py

--- poprox-db/migrations/versions/2025_02_06_1339-6da1373105ef_add_column_for_qualtrics_question_.py
+++ poprox-db/migrations/versions/2025_02_06_1339-6da1373105ef_add_column_for_qualtrics_question_.py
@@ -19,7 +19,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column("qualtrics_surveys", sa.Column("question_metadata", JSONB, nullable=True))
+    op.add_column("qualtrics_surveys", sa.Column("question_metadata_raw", JSONB, nullable=True))
 
 
 def downgrade() -> None:
