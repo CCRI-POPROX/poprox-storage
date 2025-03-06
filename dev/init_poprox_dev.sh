9d04b3c90f46113293c50bf21f95082f9d935f41
Daniel Kluver <kluve018@umn.edu>
2025-03-06 13:36:54 -0600

    change how init_ and stop_poprox_dev modify their paths to start/stop the db container

--- dev/init_poprox_dev.sh
+++ dev/init_poprox_dev.sh
@@ -2,7 +2,8 @@
 # this can be called from any folder (adjust the path as needed when sourcing the file)
 # you may find it useful to set up an alias.
 
-pushd "$(dirname "${BASH_SOURCE[0]}")"
+script_dir="${0:A:h}"
+pushd "$script_dir"
 
 export POPROX_DB_PASSWORD=$(cat db/password.txt)
 export PG_PASSWORD=$(cat db/password.txt)
