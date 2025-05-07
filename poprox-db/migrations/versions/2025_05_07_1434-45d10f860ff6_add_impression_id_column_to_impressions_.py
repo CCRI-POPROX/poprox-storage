e3537c4c8de787d47f63b6aeb5c0a5701013962b
Karl Higley <khigley@umn.edu>
2025-05-07 14:37:31 -0400

    Add `impression_id` column to `impressions` table as primary key

---
+++ poprox-db/migrations/versions/2025_05_07_1434-45d10f860ff6_add_impression_id_column_to_impressions_.py
@@ -0,0 +1,32 @@
+"""add impression_id column to impressions table
+
+Revision ID: 45d10f860ff6
+Revises: 2a18762e7703
+Create Date: 2025-05-07 14:34:22.412392
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '45d10f860ff6'
+down_revision: Union[str, None] = '2a18762e7703'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "impressions",
+        "impression_id",
+        sa.UUID,
+        primary_key=True,
+        server_default=sa.text("gen_random_uuid()")
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("impressions", "impression_id")
