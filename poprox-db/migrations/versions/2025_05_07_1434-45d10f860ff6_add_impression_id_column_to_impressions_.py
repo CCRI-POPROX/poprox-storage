fa40ba157fcd1cb07c23f6306caac850f8e40bc7
Daniel Kluver <kluve018@umn.edu>
2025-05-12 19:21:46 +0000

    fixed primary key construction and removal for impressions

--- poprox-db/migrations/versions/2025_05_07_1434-45d10f860ff6_add_impression_id_column_to_impressions_.py
+++ poprox-db/migrations/versions/2025_05_07_1434-45d10f860ff6_add_impression_id_column_to_impressions_.py
@@ -5,14 +5,15 @@ Revises: 2a18762e7703
 Create Date: 2025-05-07 14:34:22.412392
 
 """
+
 from typing import Sequence, Union
 
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '45d10f860ff6'
-down_revision: Union[str, None] = '2a18762e7703'
+revision: str = "45d10f860ff6"
+down_revision: Union[str, None] = "2a18762e7703"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -20,15 +21,11 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.add_column(
         "impressions",
-        sa.Column(
-            "impression_id",
-            sa.UUID,
-            primary_key=True,
-            server_default=sa.text("gen_random_uuid()")
-        )
-
+        sa.Column("impression_id", sa.UUID, server_default=sa.text("gen_random_uuid()")),
     )
+    op.create_primary_key("pk_impressions_impression_id", "impressions", ["impression_id"])
 
 
 def downgrade() -> None:
+    op.drop_constraint("pk_impressions_impression_id", "impressions")
     op.drop_column("impressions", "impression_id")
