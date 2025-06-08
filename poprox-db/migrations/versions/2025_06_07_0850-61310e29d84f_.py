156e876c2493da9bbbec63fe2093a86c54841d82
Karl Higley <khigley@umn.edu>
2025-06-08 07:35:37 -0400

    Appease the linter by removing unused imports

--- poprox-db/migrations/versions/2025_06_07_0850-61310e29d84f_.py
+++ poprox-db/migrations/versions/2025_06_07_0850-61310e29d84f_.py
@@ -7,10 +7,6 @@ Create Date: 2025-06-07 08:50:57.239850
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = '61310e29d84f'
 down_revision: Union[str, None] = ('f79aeb521ebb', '1d20bc70fa6d')
