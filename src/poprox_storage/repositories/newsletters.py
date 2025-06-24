dd89191e74a1ee2003d9e4bf3baa93bf4ec2c395
Karl Higley <khigley@umn.edu>
2025-06-24 15:09:33 -0400

    Apply auto-formatting

--- src/poprox_storage/repositories/newsletters.py
+++ src/poprox_storage/repositories/newsletters.py
@@ -366,8 +366,8 @@ def extract_and_flatten(newsletters: list[Newsletter], include_treatment: bool =
             record["headline"] = impression.headline
             record["subhead"] = impression.subhead
             record["recommender_name"] = newsletter.recommender_info.name if newsletter.recommender_info else ""
-            record["recommender_version"] = newsletter.recommender_info.version  if newsletter.recommender_info else ""
-            record["recommender_hash"] = newsletter.recommender_info.hash  if newsletter.recommender_info else ""
+            record["recommender_version"] = newsletter.recommender_info.version if newsletter.recommender_info else ""
+            record["recommender_hash"] = newsletter.recommender_info.hash if newsletter.recommender_info else ""
             if include_treatment:
                 record["treatment_id"] = str(newsletter.treatment_id)
             if impression.extra:
